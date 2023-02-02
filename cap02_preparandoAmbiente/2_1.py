f16cbcc1cba3c8b43675ca892f3df61fe8d5482b
Adilson Rodrigues Martins <contato.adilsonmartins@gmail.com>
2023-02-02 16:25:56 -0300

    initial commit

---
+++ cap02_preparandoAmbiente/2_1.py
@@ -0,0 +1,6 @@
+from os import system, name
+
+system("cls") if name == "nt" else system("clear")
+print(10 + 20 * 30)
+print(4 ** 2 / 30)
+print((9 ** 4 + 2) * 6 - 1)
---
+++ cap02_preparandoAmbiente/2_2.py
@@ -0,0 +1,4 @@
+from os import system, name
+
+system("cls") if name == "nt" else system("clear")
+print(10 / 3 * 10 ** 2 + 1 - 10 * 4 / 2)
