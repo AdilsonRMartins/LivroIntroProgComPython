388ffde836189db5cb9df1ca99532c4780d8ab58
Adilson Rodrigues Martins <contato.adilsonmartins@gmail.com>
2023-02-02 18:27:12 -0300

    initial commit

---
+++ cap03_variaveisEntradaDados/3_4.py
@@ -0,0 +1,5 @@
+from os import system, name
+
+system("cls") if name == "nt" else system("clear")
+salario = 1200
+print(salario > 1200)
---
+++ cap03_variaveisEntradaDados/3_6.py
@@ -0,0 +1,7 @@
+from os import system, name
+
+system("cls") if name == "nt" else system("clear")
+materia1 = 9
+materia2 = 5
+materia3 = 8
+print(materia1 >= 7 and materia2 >= 7 and materia3 >= 7)
